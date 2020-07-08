c0dbb7dc1c730e3c36d7717a13892e5d55c275dd
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-07-08 21:18:15 +0530

    added summarize method

--- neuralnet_visualize/visualize.py
+++ neuralnet_visualize/visualize.py
@@ -5,30 +5,43 @@ import graphviz as gv
 class visualizer():
     def __init__(self, title="My-Neural-Network"):
         self.title = title
-        self.network = gv.Graph(title, directory='./graphs', format='png',
+        self.color_encoding = {'input': 'yellow', 'hidden': 'green', 'output': 'red'}
+        self.network = gv.Graph(title, directory='./graphs', format='jpeg',
               graph_attr=dict(ranksep='2', rankdir='LR', color='white', splines='line'),
               node_attr=dict(label='', shape='circle', width='0.5'))
 
         self.layers = 0
         self.layer_names = list()
+        self.layer_types = list()
         self.layer_units = list()
 
     def __str__(self):
         return self.title
 
-    def add_layer(self, layer_name, nodes):
+    def add_layer(self, layer_type, nodes):
+        if self.layers == 0:
+            layer_name = layer_type.capitalize()+'_input'
+        else:
+            layer_name = layer_type.capitalize()+'_hidden'+str(self.layers)
+
+        self.layer_types.append(layer_type)
+        self.layer_names.append(layer_name)
+        self.layer_units.append(nodes)
+        self.layers = self.layers + 1
+
         with self.network.subgraph(name=f'cluster_{layer_name}') as layer:
             if nodes > 10:
                 layer.attr(labeljust='right', labelloc='bottom', label='+'+str(nodes - 10))
                 nodes = 10
-            layer.attr(body=layer_name)
 
             for i in range(nodes):
-                layer.node(f'{layer_name}_{i}', style='filled', fillcolor='yellow')
+                if self.layers == 1:
+                    color = self.color_encoding['input']
+                else:
+                    color = self.color_encoding['hidden']
+                layer.node(f'{layer_name}_{i}', style='filled', fillcolor=color)
 
-        self.layer_names.append(layer_name)
-        self.layer_units.append(nodes)
-        self.layers = self.layers + 1
+        return
 
     def _connect_layers(self, l1_nodes, l2_nodes, l1_name, l2_name):
         for l1 in range(l1_nodes):
@@ -38,23 +51,55 @@ class visualizer():
 
                 self.network.edge(n1, n2)
 
+        return
+
     def _build_network(self):
         for i in range(self.layers - 1):
-            self._connect_layers(self.layer_units[i], self.layer_units[i+1], self.layer_names[i], self.layer_names[i+1])
+            nodes1 = self.layer_units[i]
+            nodes2 = self.layer_units[i+1]
+
+            if self.layer_units[i] > 10:
+                nodes1 = 10
+            if self.layer_units[i+1] > 10:
+                nodes2 = 10
+
+            self._connect_layers(nodes1, nodes2, self.layer_names[i], self.layer_names[i+1])
+
+        return
+
+    def summarize(self):
+        title = "Neural Network Architecture"
+        print("+---------------------------------------------------------------------+")
+        print("|"+title.center(69)+"|")
+        print("+---------------------------------------------------------------------+")
+        print("|\tLayer Name\t|\tLayer Type\t|\tLayer Units\t|")
+        print("+---------------------------------------------------------------------+")
+        for i in range(self.layers):
+            print("|\t"+self.layer_names[i]+"\t|\t"+self.layer_types[i].capitalize()+"\t\t|\t"+str(self.layer_units[i])+"\t\t|")
+            print("+---------------------------------------------------------------------+")
+
+        return
 
     def visualize(self):
+        if self.layers < 2:
+            print("Cannot draw Neural Network")
+            print("Add atleast two layers to the network")
+            exit()
         self._build_network()
         self.network.view()
 
+        return
+
 if __name__ == '__main__':
-    input_nodes = 6
-    hidden_nodes = 12
+    input_nodes = 14
+    hidden_nodes = 5
     output_nodes = 4
 
     net = visualizer()
 
-    net.add_layer('input', input_nodes)
-    net.add_layer('hidden', hidden_nodes)
-    net.add_layer('output', output_nodes)
+    net.add_layer('dense', input_nodes)
+    net.add_layer('dense', hidden_nodes)
+    net.add_layer('dense', output_nodes)
 
     net.visualize()
+    net.summarize()
