3a07de7e2d375c4f1220faf7a4fad5335ab8247a
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-07-09 16:51:30 +0530

    modified setup file

--- setup.py
+++ setup.py
@@ -1,28 +1,34 @@
 from setuptools import setup, find_packages 
 
-with open('README.md') as readme_file:
-    README = readme_file.read()
+with open('DESCRIPTION.md') as readme_file:
+    DESCRIPTION = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
 
 setup_args = dict(
     name='neuralnet_visualize',
     version='0.0.2',
     description='Generate a neural network architecture Image',
     long_description_content_type='text/markdown',
-    long_description=README,
+    long_description=DESCRIPTION+"\n\n\n"+HISTORY,
     license='Apache License 2.0',
     packages=find_packages(),
     author='Anurag Peddi',
     author_email='anurag.peddi1998@gmail.com',
     keywords=['Neural', 'Network', 'Visualize', 'Graphviz'],
     url='https://github.com/AnuragAnalog/nn_visualize',
-    download_url='https://pypi.org/project/',
+    download_url='https://pypi.org/project/neuralnet-visualize/',
     classifiers=[
         "Natural Language :: English",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: Apache Software License",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Visualization",
     ],
     python_requires='>=3.6'
 )
